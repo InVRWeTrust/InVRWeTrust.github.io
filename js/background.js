7707d436fd032b50ee241b48f46df498407a93cd
neopostmodern <clemens@neopostmodern.com>
2020-07-30 18:47:12 +0200

    red blue background, changed parallax layers

--- js/background.js
+++ js/background.js
@@ -19,26 +19,6 @@ if (isIOS == 1) {
   $('head').append('<link rel="stylesheet" type="text/css" href="./css/iOS.css">');
 }
 
-var text = '';
-for(var i=0; i<100; i++) {
-  text += '<div class="prop-text">in <span class="emph">VR</span> we <span class="emph">trust</span></div> ';
-  text += '<div class="prop-text"><span class="emph">we</span> trust <span class="emph">in</span> VR</div> ';
-}
-$('#prop').html(text);
-
-function fillBack() {
-  var viewHeight = document.getElementById("container").scrollHeight;
-  console.log(viewHeight);
-  viewHeight = Math.ceil(viewHeight * 1.3);
-  $("#prop").css("height", viewHeight);
-  console.log(viewHeight);
-}
-fillBack();
-
-$(window).resize(function(){
-  fillBack();
-});
-
 //var height = $('#container').height();
 //console.log(height);
 //$("#static1").offset({top: height, left: 0 });
