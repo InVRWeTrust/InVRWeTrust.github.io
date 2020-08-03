3787d3bd3e8364dd70a11363279942b679977480
alexh-name <git@alexh.name>
2020-08-03 15:17:33 +0200

    openCall always sticky

--- js/stickers.js
+++ js/stickers.js
@@ -1,31 +1,31 @@
 var windowHeight = $(window).height();
 console.log("windowHeight: " + windowHeight);
 var backToTop = $('#backToTop');
-var openCall = $('#static1');
-var tolerance = Math.ceil( $(openCall).height() * 10 );
-console.log("windowHeight tol: " + tolerance)
-var position = $(openCall).css("position");
-var top = $(openCall).css("top");
-var left = $(openCall).css("left");
-var margin = $(openCall).css("margin");
+//var openCall = $('#static1');
+//var tolerance = Math.ceil( $(openCall).height() * 10 );
+//console.log("windowHeight tol: " + tolerance)
+//var position = $(openCall).css("position");
+//var top = $(openCall).css("top");
+//var left = $(openCall).css("left");
+//var margin = $(openCall).css("margin");
 
 function scrollCheck() {
   var scroll = $('#container').scrollTop();
-  scroll = scroll + tolerance;
+  //scroll = scroll + tolerance;
   if (scroll > windowHeight) {
     console.log("scrolled past windowHeight");
-    $(openCall).appendTo("body");
-    $(openCall).css("position", "fixed");
-    $(openCall).css("top", "2ex");
-    $(openCall).css("left", "3em");
-    $(openCall).css("margin", "0");
+    //$(openCall).appendTo("body");
+    //$(openCall).css("position", "fixed");
+    //$(openCall).css("top", "2ex");
+    //$(openCall).css("left", "3em");
+    //$(openCall).css("margin", "0");
     $(backToTop).show();
   } else {
-    $(openCall).insertAfter("#static2");
-    $(openCall).css("position", position);
-    $(openCall).css("top", top);
-    $(openCall).css("left", left);
-    $(openCall).css("margin", margin);
+    //$(openCall).insertAfter("#static2");
+    //$(openCall).css("position", position);
+    //$(openCall).css("top", top);
+    //$(openCall).css("left", left);
+    //$(openCall).css("margin", margin);
     $(backToTop).hide();
   }
 }
