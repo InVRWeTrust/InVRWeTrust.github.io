bc984c5fac98b2be7fcddd8101f1a060df76b46b
alexh-name <git@alexh.name>
2020-09-14 18:25:54 +0200

    js/contents.js -> settings.js

---
+++ settings.js
@@ -0,0 +1,40 @@
+var langs = ["en", "de"];
+
+var texts = {
+  'backToTop': [],
+  'consentInfo': ['policiesSoundcloud','policiesVimeo','privacyLink'],
+  'sliderDesc': [],
+  'saveDesc': [],
+  'saveBtn': [],
+  'trustQ': [],
+  'yes': [],
+  'no': [],
+  'allBtn': [],
+  'saveAndClose': [],
+  'consentReminder': [],
+  'intro': [],
+  'interviewFarina': [],
+  'interviewBroeckmann': [],
+  'subscribe-fold': [],
+  'co-curation-fold': [],
+  'impressum-fold': ['impressumAddress'],
+  'privacy-fold': []
+};
+
+var sounds = [
+  {
+    'title': 'Baruch Gottlieb in conversation with Clemens Schöll',
+    'url': 'https://soundcloud.com/invrwetrust/baruch-gottlieb'
+  }
+];
+var vimeos = [
+
+];
+
+var bgText = [
+  'in <span class="emph">VR</span> we <span class="emph">trust</span>',
+  'we <span class="emph">trust</span> in <span class="emph">VR</span>',
+  'we <span class="emph">trusted</span> in <span class="emph">VR</span>',
+  'we <span class="emph">will trust</span> in <span class="emph">VR</span>',
+  '<span class="emph">can</span> we <span class="emph">trust</span> in <span class="emph">VR</span>?'
+]
