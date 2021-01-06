b5eb1d3d78e3eec49602ebc07fc42beda23c3549
alexh-name <git@alexh.name>
2021-01-06 13:28:01 +0100

    enable captions for vimeo in $lang if its not de

--- js/addContents.js
+++ js/addContents.js
@@ -114,7 +114,11 @@ function addContents(div, targets, index) {
     var id = contents[index].url.match("vimeo.com/([0-9]+)")[1];
     html += '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/'
     html += id
-    html += '?color=25afe0&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>'
+    html += '?color=25afe0&title=0&byline=0&portrait=0&texttrack='
+    if ( lang != 'de' ) {
+      html += lang
+    }
+    html += '" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>'
     // put into DOM
     placeholderReplace(div, html);
     break;
